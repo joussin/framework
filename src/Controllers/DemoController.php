965e555f97f3f64d4cc9ca8cf28de5d8c9108a10
stef <joussin@live.com>
2015-09-12 23:36:31 +0200

    installation composant formulaire

--- src/Controllers/DemoController.php
+++ src/Controllers/DemoController.php
@@ -3,22 +3,27 @@ namespace Src\Controllers;
 
 use App\Lib\Controller\AbstractController;
 use Src\Entities\Demo;
-
+use Symfony\Component\Form\Forms;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 final class DemoController extends AbstractController
 {
 
 
     public  function indexAction($name){
 
-        echo $this->getContainer()->get('demoservice')->getStr();
+//        echo $this->getContainer()->get('demoservice')->getStr();
+//
+//        $product = new Demo();
+//        $product->setName('Stéphane');
+//        $product->setDate(new \DateTime('now'));
+//
+//        $em = $this->getContainer()->get('doctrine')->getEntityManager();
+//        $em->persist($product);
+//        $em->flush();
 
-        $product = new Demo();
-        $product->setName('Stéphane');
-        $product->setDate(new \DateTime('now'));
 
-        $em = $this->getContainer()->get('doctrine')->getEntityManager();
-        $em->persist($product);
-        $em->flush();
+
+
 
 
         $link = $this->getContainer()->get('router')->generateUrl('route_1',array('name'=> $name));
