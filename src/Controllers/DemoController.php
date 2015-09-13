afefcb00d087ab6dc671299ace908ee8acad350e
stef <joussin@live.com>
2015-09-13 21:43:16 +0200

    traitement formulaire

--- src/Controllers/DemoController.php
+++ src/Controllers/DemoController.php
@@ -5,26 +5,16 @@ use App\Lib\Controller\AbstractController;
 use Src\Entities\Demo;
 
 
-use Symfony\Bridge\Twig\Extension\TranslationExtension;
-use Symfony\Component\Form\Extension\Csrf\CsrfProvider\DefaultCsrfProvider;
-use Symfony\Component\Form\Extension\Csrf\CsrfProvider\SessionCsrfProvider;
-use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\Form\Forms;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
-
-use Symfony\Bridge\Twig\Extension\FormExtension;
-use Symfony\Bridge\Twig\Form\TwigRenderer;
-use Symfony\Bridge\Twig\Form\TwigRendererEngine;
-use Symfony\Component\Translation\Loader\XliffFileLoader;
-use Symfony\Component\Translation\Translator;
 
 final class DemoController extends AbstractController
 {
 
 
-    public  function indexAction($name){
+    public  function indexAction($name, Request $request){
+
 
 //        echo $this->getContainer()->get('demoservice')->getStr();
 //
@@ -36,6 +26,8 @@ final class DemoController extends AbstractController
 //        $em->persist($product);
 //        $em->flush();
 
+        $link = $this->getContainer()->get('router')->generateUrl('route_1', array('name'=>$name));
+
 
         $formFactory = Forms::createFormFactoryBuilder()->getFormFactory();
 
@@ -44,13 +36,19 @@ final class DemoController extends AbstractController
             ->add('dueDate', 'date')
             ->getForm();
 
+//        $form->handleRequest($request);
+//
+//        if ($form->isValid()) {
+//            echo 'FORM VALID';
+//            $data = $form->getData();
+//            var_dump($data);
+//        }
 
-        $link = $this->getContainer()->get('router')->generateUrl('route_1',array('name'=> $name));
 
         return  $this->render("Demo/index.html.twig",
             array(
-                'name'=> $name,
-                'link'=>$link,
+                 'name'=>$name,
+                 'link'=>$link,
                 'form' => $form->createView(),
             )
         );
