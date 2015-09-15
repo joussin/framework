fbe8873f812a8f6affedaad01c97ea1ccf617e4d
stephane <joussin@live.com>
2015-09-15 16:59:42 +0200

    création des type, des formualires reliés aux entité l'hydratation ne marche pas

--- src/Controllers/DemoController.php
+++ src/Controllers/DemoController.php
@@ -5,9 +5,9 @@ use App\Lib\Controller\AbstractController;
 use Src\Entities\Demo;
 
 
+use Src\Form\DemoType;
 use Symfony\Component\Form\Forms;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints as Assert;
 
 final class DemoController extends AbstractController
@@ -18,37 +18,40 @@ final class DemoController extends AbstractController
 
 
 //        echo $this->getContainer()->get('demoservice')->getStr();
-//
-//        $product = new Demo();
-//        $product->setName('Stéphane');
-//        $product->setDate(new \DateTime('now'));
-//
-//        $em = $this->getContainer()->get('doctrine')->getEntityManager();
-//        $em->persist($product);
-//        $em->flush();
-
         $link = $this->getContainer()->get('router')->generateUrl('route_1', array('name'=>$name));
+        $em = $this->getContainer()->get('doctrine')->getEntityManager();
 
+
+
+        $demo = new Demo();
+        $demo->setName("name de test");
+        $demo->setDate(new \DateTime('now'));
+
+        $date = $demo->getDate()->format("Y-m-d H:i:s");
+
+
+        $demoType =new DemoType();
 
         $formFactory = Forms::createFormFactoryBuilder()->getFormFactory();
-
-        $form = $formFactory->createBuilder()
-            ->add('task', 'text', array(
-                'required' => false,
-//                'constraints' => new NotBlank(),
-            ))
-            ->add('dueDate', 'date')
-            ->getForm();
-
+        $form = $formFactory->createBuilder($demoType,$demo)->getForm();
 
 
          $form->submit($request->request->get($form->getName()));
 
+        if ($request->isMethod('POST')) {
         if ($form->isValid()) {
-            echo 'FORM VALID';
-             $data = $form->getViewData();
-             var_dump($data);
-        }
+
+            $demo = $form->getViewData();
+            $em->persist($demo);
+            $em->flush();
+
+        }}
+
+
+        $demoRepository = $em->getRepository('Src\Entities\Demo');
+        $demos = $demoRepository->findAll();
+
+
 
 
         return  $this->render("Demo/index.html.twig",
@@ -56,6 +59,7 @@ final class DemoController extends AbstractController
                  'name'=>$name,
                  'link'=>$link,
                 'form' => $form->createView(),
+                'demos' => $demos,
             )
         );
     }
---
+++ src/Form/DemoType.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wal21
+ * Date: 15/09/15
+ * Time: 16:52
+ */
+
+namespace Src\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class DemoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+        $builder->add('date', 'datetime');
+    }
+
+    public function getName()
+    {
+        return 'demo';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Src\Entities\Demo',
+        ));
+    }
+}
