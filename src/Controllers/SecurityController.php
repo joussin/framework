533e72711fafec4893341e56c2945cc09055e595
stephane <joussin@live.com>
2015-09-16 17:19:28 +0200

    security component

--- src/Controllers/SecurityController.php
+++ src/Controllers/SecurityController.php
@@ -2,7 +2,16 @@
 namespace Src\Controllers;
 
 use App\Lib\Controller\AbstractController;
+use App\Lib\SomeAuthenticationListener;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestMatcher;
+use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Http\Firewall;
+use Symfony\Component\Security\Http\FirewallMap;
+
 
 final class SecurityController extends AbstractController
 {
@@ -12,6 +21,26 @@ final class SecurityController extends AbstractController
 
 
 
+
+//        $map = new FirewallMap();
+
+//        $requestMatcher = new RequestMatcher('^/secured-area/');
+//        $listener = new SomeAuthenticationListener();
+//        $listeners = array($listener);
+
+//        $exceptionListener = new \Symfony\Component\Security\Http\Firewall\ExceptionListener($this);
+
+//        $map->add($requestMatcher, $listeners, $exceptionListener);
+//
+//// the EventDispatcher used by the HttpKernel
+//        $dispatcher = new EventDispatcher();
+
+//        $firewall = new Firewall($map, $dispatcher);
+
+//        $dispatcher->addListener(KernelEvents::REQUEST, array($firewall, 'onKernelRequest'));
+//
+//
+//
         return  $this->render("Security/index.html.twig");
     }
 }
